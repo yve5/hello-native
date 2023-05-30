209c97c45349d20dffb7151dfdaa46dff1b25b2c
Yves Roll <yves.rouille@gmail.com>
2023-05-31 01:16:25 +0200

    wip

--- src/Phaser.jsx
+++ src/Phaser.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-// import Showcase from './showcase';
+import Showcase from './showcase';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#00FFFF',
   },
-  title: {
-    fontSize: 30,
-    padding: 10,
-    top: 100,
-  },
 });
 
 const Game = () => (
   <View style={styles.container}>
-    <Text style={styles.title}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua.
-    </Text>
-
-    {/*
     <Showcase />
-    */}
-
     <StatusBar style="auto" />
   </View>
 );
--- src/showcase/index.jsx
+++ src/showcase/index.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from 'react';
-import { AUTO, Game } from 'phaser';
+// import { AUTO, Game } from 'phaser';
+import { Text } from 'react-native';
 
-import Scene from './Scene';
+// import Scene from './Scene';
 
 const Container = () => {
   const container = useRef(null);
@@ -9,33 +10,47 @@ const Container = () => {
 
   useEffect(() => {
     if (!game.current) {
-      game.current = new Game({
-        type: AUTO,
-        width: 800,
-        height: 600,
-        parent: container.current,
-        physics: {
-          default: 'arcade',
-          arcade: {
-            gravity: { y: 200 },
-          },
-        },
-        scene: [Scene],
-        banner: {
-          hidePhaser: true,
-        },
-      });
+      console.log('Hello World');
+
+      //     game.current = new Game({
+      //       type: AUTO,
+      //       width: 800,
+      //       height: 600,
+      //       parent: container.current,
+      //       physics: {
+      //         default: 'arcade',
+      //         arcade: {
+      //           gravity: { y: 200 },
+      //         },
+      //       },
+      //       scene: [Scene],
+      //       banner: {
+      //         hidePhaser: true,
+      //       },
+      //     });
     }
 
-    return () => {
-      if (!game.current) {
-        game.current.destroy(true);
-        game.current = null;
-      }
-    };
+    //   return () => {
+    //     if (!game.current) {
+    //       game.current.destroy(true);
+    //       game.current = null;
+    //     }
+    //   };
   }, []);
 
-  return <div className="game" ref={container} />;
+  // return <div className="game" ref={container} />;
+
+  return (
+    <Text>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </Text>
+  );
 };
 
 export default Container;
